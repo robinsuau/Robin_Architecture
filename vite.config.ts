5fc76ce3cc2755885089e9fd3f73146c8895da9d
robinsuau <robin.suau@gmail.com>
2025-12-04 15:14:40 +0100

    Change base path in Vite config
    Update base path for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Assure que les chemins sont relatifs pour le déploiement sur GitHub Pages
-});
+  base: "/Robin_Architecture/",
+})
